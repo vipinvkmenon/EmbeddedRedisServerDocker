20e90fe934de73aa0cb1c2ada41be537077f3985
Vipin Menon <vipin.vijaykumar@sap.com>
2017-10-24 10:52:47 +0530

    jmm

--- ci/build.sh
+++ ci/build.sh
@@ -3,7 +3,12 @@
 set -e -x
 
 chmod 777 /tmp
+mkdir /tmp/redisServer
+chmod 777 /tmp/redisServer
+
 cd source-code
+cp ci/RedisServer/* /tmp/redisServer
+chmod 777 /tmp/redisServer/*
 ls
 pwd
 ./mvnw clean install 
